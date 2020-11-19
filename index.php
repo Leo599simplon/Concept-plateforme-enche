ecd31fa994c48e77a1c398676770404997c2b16d
David ADOLPHE <dadoo97441@gmail.com>
2020-11-19 08:50:54 +0400

    front

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
 
     <!-- jQuery and JS bundle w/ Popper.js -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -14,7 +15,73 @@
 </head>
 
 <body>
-    test    
+
+    <!-------------- Header ---------------->
+    <header class="container-fluid d-flex align-items-center justify-content-center">
+        <div>
+            <h1>Plateforme enchère</h1>
+        </div>
+    </header>
+    <!-------------------------------------->
+
+    <!---------------Produits --------------------->
+    <div class="container-fluid mt-5 d-flex flex-column justify-content-center align-items-center">
+
+        <!------- 1ere rangée de produits-------->
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/gopro.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/ps5.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/nespresso.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <!--------------------------------->
+
+        <!------- 2ere rangée de produits-------->
+        <div class="row mt-5">
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/reflex.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/television.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <h5>Nom du produit</h5>
+                <img src="ressources/img/velo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <!--------------------------------->
+    </div>
+    <!-------------------------------------->
+
+
 
 </body>
 
