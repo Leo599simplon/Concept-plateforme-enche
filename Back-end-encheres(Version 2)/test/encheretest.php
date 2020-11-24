16f69d722df5e129cbbe4c060849eb181c4a6a33
Leo599simplon <70998597+Leo599simplon@users.noreply.github.com>
2020-11-24 11:24:24 +0400

    Ajout de paramètres et modification en cohérence des fichiers, mise en commentaire de la fonction display et code la nouvelle proposition d'affichage

--- Back-end-encheres(Version 2)/test/encheretest.php
+++ Back-end-encheres(Version 2)/test/encheretest.php
@@ -4,37 +4,47 @@
 //"implements JsonSerializable" permet aux instances de classe d'hériter de l'interface JsonSerializable qui permet l'utilisation de methodes tel que jsonSerialization()
 class Enchere implements JsonSerializable {
 	//private = accessible qu'à la classe
+	/** Id du produit */
+	private $m_id = 0;
+	/** Titre du produit */
 	private $m_titre = "";
+	/** Description du produit */
 	private $m_desc = "";
 	/**	Prix du clic en centimes */
 	private $m_clic = 0;
 	/**	Montée de l'enchère en centimes */
-	private $m_step = 0;
+	private $m_stepprice = 0;
+	/**Prix du produit */
+	private $m_price = 0
 	/**	Date d'expiration de l'enchère */
-	private $m_time = "";
+	private $m_deadline = "";
+	/**	Augmentation du temps pour une enchère */
+	private $m_steptime = "";
 	//public = accessible en dehors de la classe
-	public function __construct($titre, $desc, $clic, $step, $prix, $time) {
-		$this->m_titre = $titre;
-		$this->m_desc  = $desc;
-		$this->m_clic  = $clic;
-		$this->m_step  = $step;
-		$this->m_prix  = $prix;
-		$this->m_time  = $time;
+	public function __construct($id, $titre, $desc, $clic, $stepprice, $price, $deadline, $steptime) {
+		$this->m_id         = $id;
+		$this->m_titre      = $titre;
+		$this->m_desc       = $desc;
+		$this->m_clic       = $clic;
+		$this->m_stepprice  = $stepprice;
+		$this->m_price      = $price;
+		$this->m_deadline   = $deadline;
+		$this->m_steptime   = $steptime;
 	}
 	
 	/** 
-		Renvoie le titre de l'enchère
+		Renvoie l'Id' de l'enchère
 	*/
-	public function getTitre() {
-		return $this->m_titre;
+	public function getId() {
+		return $this->m_id;
 	}
 	
 	/**
-		Mets à jour le prix et la durée de vie de l'enchère
+		Mets à jour le price et la durée de vie de l'enchère
 	*/
 	public function enchere() {
-		$this->m_prix += $this->m_step / 100;
-		$this->m_time += 30;
+		$this->m_price += $this->m_stepprice / 100;
+		$this->m_time += $steptime;
 	}
 	
 	/**
@@ -42,12 +52,14 @@ class Enchere implements JsonSerializable {
 	*/
 	public function jsonSerialize() {
 		return array(
-		           "m_titre" => $this->m_titre,
-		           "m_desc" => $this->m_desc,
-		           "m_clic" => $this->m_clic,
-		           "m_step" => $this->m_step,
-		           "m_prix" => $this->m_prix,
-		           "m_time" => $this->m_time
+				   "m_id"        => $this->m_id;
+		           "m_titre"     => $this->m_titre,
+		           "m_desc"      => $this->m_desc,
+		           "m_clic"      => $this->m_clic,
+		           "m_stepprice" => $this->m_stepprice,
+		           "m_price"     => $this->m_price,
+				   "m_deadline"  => $this->m_deadline,
+				   "m_steptime"  => $this->m_steptime;
 				   );
 	}
 }
