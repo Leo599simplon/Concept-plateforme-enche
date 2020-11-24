1301bba1767a146ffdc0780ec9e436a80a647141
David ADOLPHE <dadoo97441@gmail.com>
2020-11-24 18:24:04 +0400

    résolution soucis technique

--- proposition2.php
+++ proposition2.php
@@ -44,7 +44,7 @@ include('scripts/produit2.php');
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style2.css">
+    <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <!-- jQuery and JS bundle w/ Popper.js -->
