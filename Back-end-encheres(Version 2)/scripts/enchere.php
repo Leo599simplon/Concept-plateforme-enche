e7eda550fc25e6cb7084d5b534547396ea9084f6
Leo599simplon <70998597+Leo599simplon@users.noreply.github.com>
2020-11-24 15:30:02 +0400

    Avancement dans la version 2 du Backend

--- Back-end-encheres(Version 2)/scripts/enchere.php
+++ Back-end-encheres(Version 2)/scripts/enchere.php
@@ -4,24 +4,36 @@
 //"implements JsonSerializable" permet aux instances de classe d'hériter de l'interface JsonSerializable qui permet l'utilisation de methodes tel que jsonSerialization()
 class Enchere implements JsonSerializable {
 	//private = accessible qu'à la classe
+	/** Id du produit */
+	private $m_id = 0;
+	/** Titre du produit */
 	private $m_titre = "";
+	/** Description du produit */
 	private $m_desc = "";
 	/**	Prix du clic en centimes */
 	private $m_clic = 0;
 	/**	Montée de l'enchère en centimes */
-	private $m_step = 0;
+	private $m_stepprice = 0;
+	/**Prix du produit */
+	private $m_price = 0;
 	/**	Date d'expiration de l'enchère */
-	private $m_time = "";
+	private $m_deadline = "";
+	/**	Augmentation du temps pour une enchère */
+	private $m_steptime = "";
+	/** Image du produit */
+	private $m_image = '';
 	//public = accessible en dehors de la classe
-	public function __construct($titre, $desc, $clic, $step, $prix, $time) {
-		$this->m_titre = $titre;
-		$this->m_desc  = $desc;
-		$this->m_clic  = $clic;
-		$this->m_step  = $step;
-		$this->m_prix  = $prix;
-		$this->m_time  = $time;
+	public function __construct($id, $titre, $desc, $clic, $stepprice, $price, $deadline, $steptime, $image) {
+		$this->m_id         = $id;
+		$this->m_titre      = $titre;
+		$this->m_desc       = $desc;
+		$this->m_clic       = $clic;
+		$this->m_stepprice  = $stepprice;
+		$this->m_price      = $price;
+		$this->m_deadline   = $deadline;
+		$this->m_steptime   = $steptime;
+		$this->m_image      = $image;
 	}
-	
 	/**
 		Écrit le code HTML pour afficher l'enchère
 	*/
@@ -35,20 +47,20 @@ class Enchere implements JsonSerializable {
         echo "                <p class=\"card-text\">" . $this->m_desc . "</p>";
         echo "            </div>";
         echo "            <div class=\"card\">";
-        echo "                <img class=\"card-img-top\" src=\"../media/iphone.jpg\" alt=\"Card image cap\">";
+        echo "                <img class=\"card-img-top\" src=\"" . $this->m_image . "\" alt=\"Card image cap\">";
         echo "                <div class=\"card-body\">";
         echo "                    <h5 class=\"card-title\">Prix du clic: " . $this->m_clic . "cts/clic</h5>";
         echo "                    <p class=\"card-text\"></p>";
         echo "                </div>";
         echo "                <div class=\"card-body\">";
-        echo "                    <h5 class=\"card-title\">Prix de l'enchère: +" . $this->m_step . "cts/clics</h5>";
+        echo "                    <h5 class=\"card-title\">Prix de l'enchère: +" . $this->m_stepprice . "cts/clics</h5>";
         echo "                    <p class=\"card-text\"></p>";
         echo "                </div>";
 
         echo "                <div class=\"card-footer\">";
-        echo "                    <h5 class=\"card-title\">Durée: " . date("H:i:s", - $this->m_time - time() -3600) . "</h5>";
+        echo "                    <h5 class=\"card-title\">Durée: " . date("H:i:s", - $this->m_deadline - time() -3600) . "</h5>";
 
-        echo "                    <div class=\"prix text-center\">" . $this->m_prix . " € </div><br>";
+        echo "                    <div class=\"prix text-center\">" . $this->m_price . " € </div><br>";
         //echo "                    <small class=\"text-muted\">Last updated 3 mins ago</small>";
 
         echo "                    <form metod=\"post\">";
@@ -56,7 +68,7 @@ class Enchere implements JsonSerializable {
 		/* formmethod="post" : le paramètre "enchere" n'apparaît pas dans l'URL.
 		   Actualiser la page fera apparaître un message d'avertissement pour éviter de renvoyer une enchère par erreur
 		   ----->         ----->         ----->         ----->         ----->         ----->         ----->      vvvvvvvvvvvvvvvvvvv */
-        echo "                    		<input type=\"hidden\" name=\"enchere\" value=\"" . $this->m_titre . "\" formmethod=\"post\">";
+        echo "                    		<input type=\"hidden\" name=\"enchere\" value=\"" . $this->m_id . "\" formmethod=\"post\">";
         echo "                    	</button>";
         echo "                    </form>";
 
@@ -68,18 +80,18 @@ class Enchere implements JsonSerializable {
 	}
 	
 	/** 
-		Renvoie le titre de l'enchère
+		Renvoie l'Id' de l'enchère
 	*/
-	public function getTitre() {
-		return $this->m_titre;
+	public function getId() {
+		return $this->m_id;
 	}
 	
 	/**
-		Mets à jour le prix et la durée de vie de l'enchère
+		Mets à jour le price et la durée de vie de l'enchère
 	*/
 	public function enchere() {
-		$this->m_prix += $this->m_step / 100;
-		$this->m_time += 30;
+		$this->m_price += $this->m_stepprice / 100;
+		$this->m_time += $steptime;
 	}
 	
 	/**
@@ -87,12 +99,15 @@ class Enchere implements JsonSerializable {
 	*/
 	public function jsonSerialize() {
 		return array(
-		           "m_titre" => $this->m_titre,
-		           "m_desc" => $this->m_desc,
-		           "m_clic" => $this->m_clic,
-		           "m_step" => $this->m_step,
-		           "m_prix" => $this->m_prix,
-		           "m_time" => $this->m_time
+				   "m_id"        => $this->m_id,
+		           "m_titre"     => $this->m_titre,
+		           "m_desc"      => $this->m_desc,
+		           "m_clic"      => $this->m_clic,
+		           "m_stepprice" => $this->m_stepprice,
+		           "m_price"     => $this->m_price,
+				   "m_deadline"  => $this->m_deadline,
+				   "m_steptime"  => $this->m_steptime,
+				   "m_image"     => $this->m_image
 				   );
 	}
 }
